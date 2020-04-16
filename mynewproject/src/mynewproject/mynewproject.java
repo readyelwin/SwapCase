a4aa2e0bda893b13dc970a9cc3e013950e2d1e23
Marie Ready <readyelwin@gmail.com>
2020-04-16 05:04:59 -0400

    add input from file.

--- mynewproject/src/mynewproject/mynewproject.java
+++ mynewproject/src/mynewproject/mynewproject.java
@@ -1,23 +1,34 @@
 package mynewproject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 public class mynewproject{
 
 
-	public static void main(String[] args) {
-		
-		System.out.println("Enter Your name");
-		
-		Scanner in = new Scanner(System.in);
-		//String s = in.nextLine();
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		
-		int i = 0;
-		do {
-			System.out.println("i is: " + i);
-			i++;
-		} while(i<17); 
+		Scanner in = new Scanner(new File ("students.txt"));
+		
+		List<String> students = new ArrayList<String>();
+		
+	
+		while(in.hasNextLine()) {
+			students.add(in.nextLine());
+			
 			
 		}
+		
+		for(int i = 0; i < students.size(); i++) {
+			System.out.println("name: " + students.get(i));
+		}
+				in.close();
+	}
+		
+		
 		
 		
 		
