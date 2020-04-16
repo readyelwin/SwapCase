987c0f5af68b25f61d5d7f867a86d38337715914
Marie Ready <readyelwin@gmail.com>
2020-04-16 03:32:34 -0400

    initial commit

---
+++ mynewproject/src/mynewproject/mynewproject.java
@@ -0,0 +1,28 @@
+package mynewproject;
+import java.util.Scanner;
+public class mynewproject{
+
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter Your name");
+		
+		Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+		
+		int i = 0;
+		do {
+			System.out.println("i is: " + i);
+			i++;
+		} while(i<17); 
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+
